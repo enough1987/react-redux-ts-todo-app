0b7664f8ac29d7e6867df74e548f3e8bc17b7962
otilha <otilha@corelogic.com>
2018-06-22 15:38:20 +0300

    add statefull form and validation

--- src/components/articles/components/Article-add.tsx
+++ src/components/articles/components/Article-add.tsx
@@ -9,30 +9,75 @@ interface ArticleAddProps extends React.Props<never> {
     addArticle: (article: Article) => {};
 }
 
-class ArticleAdd extends React.Component<ArticleAddProps> {
+interface ArticleAddState {
+    name: string;
+    author: string;
+    isValid: boolean;
+}
+
+const BASE_ARTICLE_STATE = {
+    name: '',
+    author: '',
+    isValid: true,
+};
+
+class ArticleAdd extends React.Component<ArticleAddProps, ArticleAddState> {
+
+    constructor(props: ArticleAddProps) {
+        super(props);
+        this.state = BASE_ARTICLE_STATE;
+    }
+
     public render() {
+        const { name, author, isValid } = this.state;
         return (
             <div>
                 <p> Name :
-                    <input type='text'
-                           ref={(input) => (this as any).nameInput = input}  />
+                    <input
+                        type='text'
+                        className='form-control'
+                        id='name'
+                        value={name}
+                        onChange={this.handleChange} />
                 </p>
                 <p> Author :
-                    <input type='text'
-                           ref={(input) => (this as any).authorInput = input} />
+                    <input
+                        type='text'
+                        className='form-control'
+                        id='author'
+                        value={author}
+                        onChange={this.handleChange} />
                 </p>
                 <input type='button' value='Add'
-                       onClick={() => {
-                           this.props.addArticle({
-                               name: (this as any).nameInput.value,
-                               author: (this as any).authorInput.value,
-                           });
-                           (this as any).nameInput.value = '';
-                           (this as any).authorInput.value = '';
-                       }} />
+                       onClick={this.handleSubmit} />
+                { !isValid ? <p>new article is not valid </p> : '' }
             </div>
         );
     }
+
+    private handleChange = (event: any) => {
+        this.setState({
+            ...this.state,
+            [event.target.id]: event.target.value,
+        });
+    }
+
+    private handleSubmit = (event: any) => {
+        const { name, author } = this.state;
+        let isValid = true;
+        if (!name || !author) {
+            isValid = false;
+            this.setState({
+                ...BASE_ARTICLE_STATE,
+                isValid,
+            });
+            console.log(this.state.name, this.state.author, this.state.isValid);
+            return;
+        }
+        this.props.addArticle({ name, author });
+        this.setState(BASE_ARTICLE_STATE);
+    }
+
 }
 
 const mapDispatchToProps = () => {
