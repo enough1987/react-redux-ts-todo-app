c9e173c754b1ccab9da267cf0dc09296d2d7abad
otilha <otilha@corelogic.com>
2018-06-23 09:59:56 +0300

    redux-logger is only for develop

--- src/+store/store.tsx
+++ src/+store/store.tsx
@@ -3,11 +3,17 @@ import articlesReducer from '../components/articles/+store/articles.reducer';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
+const middlewares = [];
+
+if (process.env.NODE_ENV === `development`) {
+    middlewares.push(logger);
+}
+
 const store = createStore(combineReducers({
         articles: articlesReducer,
     }),
     applyMiddleware(
-        logger,
+        ...middlewares,
         thunk,
     ),
 );
