8dbb5d6bcc6bc8ca9202608745570aefe8c37f15
otilha <otilha@corelogic.com>
2018-06-23 10:29:27 +0300

    redux-logger options

--- src/+store/store.tsx
+++ src/+store/store.tsx
@@ -1,11 +1,16 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import articlesReducer from '../components/articles/+store/articles.reducer';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 
 const middlewares = [];
 
 if (process.env.NODE_ENV === `development`) {
+    const logger = createLogger({
+        collapsed: true,
+        duration: true,
+    });
+
     middlewares.push(logger);
 }
 
